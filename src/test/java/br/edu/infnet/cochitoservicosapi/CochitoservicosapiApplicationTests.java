386e5e6e2098b5cd14aed9f244594baf359bfc72
Adriel Cochito <adrielcochito.dev@gmail.com>
2025-09-14 13:02:13 -0300

    Ajustes Tests

--- src/test/java/br/edu/infnet/cochitoservicosapi/CochitoservicosapiApplicationTests.java
+++ src/test/java/br/edu/infnet/cochitoservicosapi/CochitoservicosapiApplicationTests.java
@@ -2,10 +2,20 @@ package br.edu.infnet.cochitoservicosapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import br.edu.infnet.cochitoservicosapi.client.AwesomeCepFeignClient;
+import br.edu.infnet.cochitoservicosapi.client.OpenRouteFeignClient;
 
 @SpringBootTest
 class CochitoservicosapiApplicationTests {
 
+    @MockBean
+    private AwesomeCepFeignClient awesomeCepFeignClient;
+    
+    @MockBean
+    private OpenRouteFeignClient openRouteFeignClient;
+    
 	@Test
 	void contextLoads() {
 	}
--- src/test/java/br/edu/infnet/cochitoservicosapi/distancia/DistanciaServiceTest.java
+++ src/test/java/br/edu/infnet/cochitoservicosapi/distancia/DistanciaServiceTest.java
@@ -30,7 +30,7 @@ public class DistanciaServiceTest {
     
     private DistanciaService distanciaService;
     
-    private final String apiKey = "test-api-key"; // Valor fixo para testes
+    private final String apiKey = "eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6ImYyYzNkMDY3YzI1ODQ0ODhhMDc3ZmNjNTI1NmY1N2Y1IiwiaCI6Im11cm11cjY0In0%3D"; // Valor fixo para testes
     
     @BeforeEach
     void setUp() {
